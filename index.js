7b8524859435252f4c57f04a8ee507a49ff412f9
nittish03 <nittishbaboria03@gmail.com>
2024-10-14 00:39:36 +0530

    FINAL TOUCH

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-let a=document.getElementById("cb");
-let mail=document.getElementById("mail");
-let password=document.getElementById("password");
-let button=document.getElementById("button");
+let a=document.getElementById("cbL");
+let mailL=document.getElementById("mailL");
+let passwordL=document.getElementById("passwordL");
+let buttonL=document.getElementById("buttonL");
 function showP(){
-    if(document.getElementById("cb").checked){
-        document.getElementById("password").type="text";
+    if(document.getElementById("cbL").checked){
+        document.getElementById("passwordL").type="text";
         console.log("checked");
     }else{
-        document.getElementById("password").type="password";
+        document.getElementById("passwordL").type="password";
         console.log("not checked");
     }
 }
-document.getElementById("cb").onclick=showP;
-button.onclick=()=>{
+document.getElementById("cbL").onclick=showP;
+buttonL.onclick=(e)=>{
+e.preventDefault();
 alert("thank you for loging in");
-password.value="";
-mail.value="";
+passwordL.value="";
+mailL.value="";
 }
